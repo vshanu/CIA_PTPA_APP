6e09a878e5461b45ffd0442dc927fdfbbae5590e
Vishal Anumolu <vshanu@outlook.com>
2019-03-09 14:29:18 -0500

    Visual changes to Login

--- PTPA/src/app/components/login-component/login-component.component.ts
+++ PTPA/src/app/components/login-component/login-component.component.ts
@@ -4,7 +4,9 @@ import { AuthService } from '../../Services/LoginAuth/auth.service';
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
-  styleUrls: ['./login-component.component.css']
+  styleUrls: ['./login-component.component.css',
+  './login-component.component.util.css'
+]
 })
 export class LoginComponentComponent implements OnInit {
 
